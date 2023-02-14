d1c4bcfe0a5a4afded57f41724349a26428f940c
David Faletto <davidfaletto@gmail.com>
2023-02-14 08:27:14 -0300

    corrigiendo algunas cosillas

---
+++ app.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingrese su nombre ");
+let apellido = prompt("Ingrese su apellido");
+
+
+if((nombre != "") && (apellido != "")){
+    alert("Su nombre es "+nombre+ "\napellido"+apellido);
+}else{
+    alert("Debe ingresar su nombre y apellido")
+}
